d3713322c328009233261d30b7767155dfaa6d3e
Luis Schekerka <luis@Luiss-MacBook-Pro.local>
2020-12-02 08:45:48 +0100

    folder and file structure for basic components

---
+++ src/components/Player.js
@@ -0,0 +1,9 @@
+const Player = () => {
+    return (
+        <div className="player-container">
+            <h1>Player</h1>
+        </div>
+    )
+}
+
+export default Player;
---
+++ src/components/Song.js
@@ -0,0 +1,9 @@
+const Song = () => {
+    return (
+        <div className="song-container">
+            <h1>Song</h1>
+        </div>
+    )
+}
+
+export default Song;
