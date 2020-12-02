85579fb83e59349429206f2e3270df31a5993659
Luis Schekerka <luis@Luiss-MacBook-Pro.local>
2020-12-02 14:07:09 +0100

    player can stop a song due to an if else statement with the isplaying state

--- src/components/Player.js
+++ src/components/Player.js
@@ -9,13 +9,19 @@ import {
   faAngleRight,
 } from "@fortawesome/free-solid-svg-icons";
 
-const Player = ({ currentSong }) => {
+const Player = ({ currentSong, isPlaying, setIsPlaying }) => {
   // References
   const audioRef = useRef(null);
 
   // Event Handlers
   const playSongHandler = () => {
-    audioRef.current.play();
+    if (isPlaying) {
+      audioRef.current.pause();
+      setIsPlaying(!isPlaying);
+    } else {
+      audioRef.current.play();
+      setIsPlaying(!isPlaying);
+    }
   };
 
   return (
